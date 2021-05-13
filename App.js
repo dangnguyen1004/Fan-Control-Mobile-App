f59115b626048b2977ab7422343bf14aebeea779
dangnguyen1004 <nguyendangit04@gmail.com>
2021-05-13 16:21:25 +0700

    dummny code

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import React, {Component} from 'react'
-import {View, Text, StyleSheet
+import React, { Component } from 'react'
+import {
+  View, Text, StyleSheet
 
 } from 'react-native'
 
 export default class App extends Component {
-  
+
   render() {
     return (
-      <View style = {css.view}>
-          <Text>HELLO REACT NATIVE</Text>
+      <View style={css.view}>
+        <Text>HELLO REACT NATIVE</Text>
       </View>
-      );
+    );
   }
 }
 var css = StyleSheet.create({
   view: {
-    flex:1,
-    justifyContent:'center',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center'
   }
 });
