a0b67948adec2f0e3c30763386bc3eaae836a4be
Duy Nguyen Quoc <47276171+nqqduy@users.noreply.github.com>
2021-05-15 12:04:29 +0700

    Add files via upload

---
+++ components/ControlDevice.js
@@ -0,0 +1,164 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import fan from '../assets/fan.jpg'
+import dieuhoa from '../assets/dieuhoa.jpg'
+
+import { AntDesign } from '@expo/vector-icons';
+export default class ControlDevice extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <AntDesign name="arrowleft" size={30} color="white" style={{marginTop:25}}/>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.body}>      
+          <View style={styles.content}>
+            <Text style={styles.control}>Control <Text style={styles.nameRoom}>Room 101 - H6</Text> </Text>
+          </View> 
+          <TouchableOpacity style={styles.add}>
+            <View>
+            
+              <View >
+                <Text style={{textAlign:'center', color:'blue', fontWeight:'bold' }}>ADD NEW DEVICE</Text>
+              </View>
+            
+          </View>
+          </TouchableOpacity>
+
+          <View style={styles.other}>
+            <View style={styles.areafan}>
+              <View style={styles.fan}>
+              <Text style={{color:'gray', textAlign:'center'}}>F1205 - TOP</Text>
+              <Image source={fan} style={styles.imageFan} ></Image>
+              <TouchableOpacity> 
+                <Text style={{fontWeight:'bold', fontSize:20, color:'blue', textAlign:'center'}}>control</Text>
+              </TouchableOpacity> 
+            </View>
+
+              <View style={styles.fan}>
+            <Text style={{color:'gray', textAlign:'center'}}>F1205 - BOTTOM</Text>
+              <Image source={fan} style={styles.imageFan} ></Image>
+              <TouchableOpacity> 
+              <Text style={{fontWeight:'bold', fontSize:20, color:'blue', textAlign:'center'}}>control</Text> 
+              </TouchableOpacity> 
+            </View>
+          
+          </View>
+          <View style={styles.areaOther}>
+            <View style={styles.fan}>
+                <Text style={{color:'gray', textAlign:'center'}}>F1205 - TOP</Text>
+                <Image source={dieuhoa} style={styles.imageDieuhoa} ></Image>
+                <TouchableOpacity> 
+                  <Text style={{fontWeight:'bold', fontSize:20, color:'blue', textAlign:'center'}}>control</Text>
+                </TouchableOpacity> 
+              </View>
+
+              <View style={styles.fan}>
+              <Text style={{color:'gray', textAlign:'center'}}>F1205 - BOTTOM</Text>
+                <Image source={dieuhoa} style={styles.imageDieuhoa} ></Image>
+                <TouchableOpacity> 
+                <Text style={{fontWeight:'bold', fontSize:20, color:'blue', textAlign:'center'}}>control</Text> 
+                </TouchableOpacity> 
+              </View>
+          </View>
+          <View style={styles.areaBottom}>
+
+          </View>
+        </View>
+        
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+  },
+  body: {
+    flex:9,
+    width:'100%',
+    height:'100%',
+  },
+  header:{
+    flex:1,
+    backgroundColor:'blue'
+  },
+  content: {
+   flex: 1,
+   //marginTop: 40
+  },
+  control: {
+    color:'blue',
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingLeft:50,
+  },
+  nameRoom: {
+    fontSize:15,
+    color:'gray'
+  },
+  add: {
+    flex:1, //e6eaeb
+    justifyContent:'center',
+    backgroundColor: 'white',
+    marginLeft: 80,
+    marginRight:80,
+    borderRadius:100,
+    shadowColor:'black',
+    shadowOpacity:0.4,
+    shadowRadius: 5,
+    shadowOffset: {width:0, height:4},
+    elevation: 9,
+  },
+  other: {
+    flex: 9,
+  },
+  areafan: {
+    flex:2,
+    flexDirection:'row',
+    alignItems: 'center' ,
+    
+    justifyContent: 'space-around'
+  },
+  areaOther:{
+    flex:2,
+    flexDirection:'row',
+    justifyContent: 'space-around'
+   
+  },
+  areaBottom:{
+    flex:1
+  },
+  fan:{
+    width: 165,
+    height: 155,
+  
+
+    borderTopColor: 'black',
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderRightColor: 'black',
+    borderRightWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderLeftColor: 'black',
+    borderLeftWidth: StyleSheet.hairlineWidth,
+
+    borderRadius:20
+  },
+  imageFan: {
+    width:100,
+    height:100,
+    marginLeft:30
+  },
+  imageDieuhoa:{
+    width:150,
+    height:100,
+    marginLeft:8
+  }
+});
+
